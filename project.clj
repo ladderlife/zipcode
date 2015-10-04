40f12d8e7bdf5e6c5cb90eabf82c6769f7d3ab73
Kijun Seo <kijunseo@gmail.com>
2015-10-04 18:23:33 +0900

    update project info

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject zipcode "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject zipcode "0.1.0"
+  :description "Converts Zipcode to State (US only)"
+  :url "https://github.com/ladderlife/zipcode"
+  :license {:name "The MIT License (MIT)"
+            :url "https://opensource.org/licenses/MIT"}
+  :signing {:gpg-key "kijunseo@gmail.com"}
+  :deploy-repositories [["clojars" {:creds :gpg}]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clojure-csv "2.0.1"] ])

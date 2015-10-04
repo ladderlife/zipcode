e9c54a3abb6bb1c5ae04f30b24f40c0dca8be871
Kijun Seo <kijunseo@gmail.com>
2015-10-04 18:33:41 +0900

    use io/resource to locate file

--- src/zipcode/core.clj
+++ src/zipcode/core.clj
@@ -1,6 +1,7 @@
 (ns zipcode.core
-  (:require [clojure-csv.core :as csv]))
+  (:require [clojure.java.io :as io]
+            [clojure-csv.core :as csv]))
 
-(def zipcode-db (into {} (csv/parse-csv (slurp "resources/zipcode-database.csv"))))
+(def zipcode-db (into {} (csv/parse-csv (slurp (io/resource "zipcode-database.csv")))))
 
 (defn zipcode->state [zipcode] (zipcode-db (str zipcode)))

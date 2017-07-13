713a3f996c313fba56f6e940b0107a33a665da44
Ambroise Piganeau <ambroisep@users.noreply.github.com>
2017-07-13 11:01:03 -0700

    release 0.1.0 (#2)

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject zipcode "0.1.3"
+(defproject com.ladderlife/zipcode "0.1.0"
   :description "Converts Zipcode to State (US only)"
   :url "https://github.com/ladderlife/zipcode"
   :license {:name "The MIT License (MIT)"
             :url "https://opensource.org/licenses/MIT"}
-  :signing {:gpg-key "kijunseo@gmail.com"}
-  :deploy-repositories [["clojars" {:creds :gpg}]]
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clojure-csv "2.0.1"] ])
+  :repositories [["clojars" {:sign-releases false}]]
+  :jar-exclusions [#".DS_Store" #"test" #"README" #"LICENSE"]
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]])

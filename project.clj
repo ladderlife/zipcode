b1b74e64d7c0be2c47e01aeb49abe425425136c6
nick hansen <n-hansen@users.noreply.github.com>
2018-01-30 16:43:47 -0800

    version bump (#4)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ladderlife/zipcode "0.1.0"
+(defproject com.ladderlife/zipcode "0.2.0"
   :description "Converts Zipcode to State (US only)"
   :url "https://github.com/ladderlife/zipcode"
   :license {:name "The MIT License (MIT)"
